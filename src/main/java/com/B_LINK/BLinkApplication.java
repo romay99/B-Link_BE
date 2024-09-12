404c9bccbebcc9fccc80f907107a5a115779df03
LeeSooBin <romaydev@naver.com>
2024-09-12 16:26:34 +0900

    First Commit

---
+++ src/main/java/com/B_LINK/BLinkApplication.java
@@ -0,0 +1,13 @@
+package com.B_LINK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/B_LINK/BLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.B_LINK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
