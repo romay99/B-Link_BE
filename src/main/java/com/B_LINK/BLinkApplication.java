8b6ef7e8d330bf925951f57aeedfc85ac9ab8e26
LeeSooBin <romaydev@naver.com>
2024-09-12 18:03:19 +0900

    Feat : UserEntity 추가 , 내부 작성해야함

--- src/main/java/com/B_LINK/BLinkApplication.java
+++ src/main/java/com/B_LINK/BLinkApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BLinkApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BLinkApplication.class, args);
 	}
-
 }
